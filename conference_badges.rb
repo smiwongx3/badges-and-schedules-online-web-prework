935310ef468e53bc55f73344b3ae1b74d02bffc0
Samantha Wong <smiwong920@gmail.com>
2019-02-01 21:21:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,4 +3,14 @@ def badge_maker(name)
 end
 
 def badge_batch_creator(array)
-  
+  names_on_list = array.size 
+  names_off_list = 0 
+loop do 
+    if names_off_list < names_on_list
+        puts "Taking out #{array[names_off_list]}"
+        names_off_list += 1
+    else
+        break
+    end
+end
+
